5e151f78738383fb7025e677cac528479ba56e3d
RAGprog <neo.ion@mail.ru>
2020-08-10 12:52:38 +0300

    Добавлен общий таймер, небольшие оптимизации, setStat = r2, промежуточные метки, откл. мигание; README на русском, обновление текста.

--- README.md
+++ README.md
@@ -2,13 +2,39 @@
 
 The current version is for the cheap but old __at90s2313__ or __attiny2313__ (_in at90s2313 mode_).  
 
-* Freq. = 16MHz
-* 2 LEDs on PB0 and PB1 (Anode to pin)
-* 1 button from PD2 to GND
+* Freq. = 16MHz (changeble)
+* 2 LEDs on PB0 and PB1 (Anode to pin, cathode to GND) (changeble)
+* 1 button from PD2 to GND  
+
+optional:
+* 1 button from RESET to GND
 
 If you have Proteus, then you can run the attached simulation.
 
 You can change the program to suit your needs or to another controller. _Please share your code._  
 I'll be glad to contact you for any of your questions.
+
+_tstu-no@inbox.ru_
+
+----
+
+# Реализация таймера Pomodoro на микроконтроллере AVR
+
+Текущая версия – для дешевого, но старого __at90s2313__ или __attiny2313__ (_в режиме at90s2313_).  
+
+* Частота = 16MHz (изменяемо)
+* 2 светодиода к PB0 и PB1 (анод к выводу МК, катод к общему выводу) (изменяемо)
+* 1 кнопка между PD2 и общим выводом  
+
+опционально:
+* 1 кнопка между RESET и общим выводом
+
+_\* общий вывод = GND_
+
+Если у вас есть Proteus, то вы можете запустить приложенную симуляцию.
+
+Вы можете изменить программу под ваши цели или для использования с другим МК. _Пожалуйста, делитесь вашим кодом._  
+Вам захотелось задать вопрос, получить разъяснения, возникли сложности – буду рад связаться с вами.
+
 
 _tstu-no@inbox.ru_
--- main.asm
+++ main.asm
@@ -7,8 +7,12 @@
 ; Compiler:  AVRASM (Proteus)
 ;====================================================================
 
+; [~] Отсчет времени
+; [ ] Срабатывание по времени после прерывания таймера
+; [ ] ШИМ (1. Красный светодиод)
+
 ;====================================================================
-; DEFINITIONS
+; DEFINITIONS	|	Определения
 ;====================================================================
 
 #include "const.inc" ;.INCLUDE 
@@ -20,29 +24,31 @@
 freq 16	;MHz
 testmode 1 ; 1 = test mode (3s), 0 = prod. mode (25min)
 
-.equ MCUCR_pint0f =	sleep_e << SE |	sleep_m << SM |	0 << ISC11 |	0 << ISC10 |	1 << ISC01 |	0 << ISC00
-.equ MCUCR_pint0r =	sleep_e << SE |	sleep_m << SM |	0 << ISC11 |	0 << ISC10 |	1 << ISC01 |	1 << ISC00
-		; ...Interrupt 0 Sense Control 10 - falling edge, 11 - rising, 00 - Low lvl
+
 ;====================================================================
-; VARIABLES
+; VARIABLES	|	Переменные
 ;====================================================================
 .def temp = r16
-.def countHH = r25
+.def countHH = r20
 .def countH = r17
 .def countL = r18
 .def one = r19
 
-.def temp_blink = r23
+;.def temp_blink = r23
 
-;.def flags = r20	; 7|6|5|4|3|2|[pushTimer]|[setupState]
-.def setStatus = r20
+.def setStatus = r2
 .def mode = r21	; 1 = 1s, 2 = 3s
 .def bmode = r22	; modes for button
 
+;general timer
+.def timeL = r23
+.def timeH = r24
+.def timeHH = r25
 
+; zyx = 31:26
 
 ;====================================================================
-; RESET and INTERRUPT VECTORS
+; RESET and INTERRUPT VECTORS	|	Сброс и векторы прерываний
 ;====================================================================
 
       ; Reset Vector
@@ -65,16 +71,19 @@ testmode 1 ; 1 = test mode (3s), 0 = prod. mode (25min)
 EXT_INT0: ;________________________________
 	; noise reduction:
 	
+	mov temp, setStatus
 	; go to timer
-	cpi setStatus, 0
+	cpi temp, 0
 	breq int0_push
 	
 	; button released, do smth, another 1 timer ckl skip
-	cpi setStatus, 3
+	cpi temp, 3
 	breq int0_release
+	mov setStatus, temp
 reti
 	int0_push:
-		inc setStatus	; status 1
+		inc temp	; status 1
+		mov setStatus, temp
 reti
 	int0_release:
 		;inc setStatus	; что-то сделать с этим: задержка откладывается до выполнения задачи mode
@@ -95,7 +104,25 @@ TIM_OVF0: ;________________________________
 	; in temp, sreg	; status reg saving
 	; push temp
 	
-	
+	; general timer
+	dec timeL
+	;brne time	; перейти на метку "time", если не было перехода через ноль
+	sbci timeH, 0	; отнять с остатком (отнимет 1 только при переходе через ноль)
+	;brne time
+	sbci timeHH, 0
+	; Проверка окончания суток:
+	cpi timeL, LOW(day)	; сравнить
+	brne time	;	перейти на метку "time", если не равны
+	cpi timeH, BYTE2(day)
+	brne time
+	cpi timeH, BYTE3(day)
+	brne time
+	; Проверить правильность сброса таймера [ ]:
+	ldi timeL, 255
+	ldi timeH, 255
+	ldi timeHH, 255
+
+	time:
 
 	; modes
 	; 1	- 4 x 25
@@ -129,36 +156,42 @@ TIM_OVF0: ;________________________________
 	
 	;push r5		; перенесено выше, т.к. в прот.случае придется повторять в ветках
 	
-	
+	mov temp, setStatus
 	; int0 noise skiping, falling
-	cpi setStatus, 1
+	cpi temp, 1
 	breq tim0_int0_1
-	cpi setStatus, 2
+	cpi temp, 2
 	breq tim0_int0_2
 	; int0 noise skiping, rising
-	cpi setStatus, 4
+	cpi temp, 4
 	breq tim0_int0_4
-	cpi setStatus, 5
+	cpi temp, 5
 	breq tim0_int0_5
+
+	mov setStatus, temp
 	
 	sei
 reti
 	tim0_int0_1:
-		inc setStatus
+		inc temp
+		mov setStatus, temp
 		sei
 reti
 	tim0_int0_2:
-		inc setStatus
+		inc temp
+		mov setStatus, temp
 		; rising int:
 		tout	MCUCR,	MCUCR_pint0r
 		sei
 reti
 	tim0_int0_4:
-		inc setStatus
+		inc temp
+		mov setStatus, temp
 		sei
 reti
 	tim0_int0_5:
-		clr setStatus	; first state
+		clr temp	; first state
+		mov setStatus, temp
 		; faling int:
 		tout	MCUCR,	MCUCR_pint0f
 		sei
@@ -200,12 +233,14 @@ reti
 		mov temp, r4	; temp recovery
 		sei
 reti
+	mode_2:
+	rjmp mode_2j
 	mode_3:
 	rjmp mode_3j
 	; ldi r30, LOW(mode_3j)
 	; ldi r31, HIGH(mode_3j)
 	; ijmp	; переход по адресу в rZ(jump to address in rZ)
-	mode_2:	; 1s + other LED
+	mode_2j:	; 1s + other LED
 		mov r4, temp	; saving temp
 		clr temp
 		
@@ -253,15 +288,15 @@ reti
 		add countL, one		; true +1
 		adc countH, temp	; +carry	(temp = 0)
 		
-		add temp_blink, one
-		cpi temp_blink, s1
-		brlo skip_bl
-		cbi Portb, green_pnum
-		skip_bl:
-		cpi temp_blink, s1 * 2
-		brlo skip_bl2
-		sbi portB, green_pnum
-		skip_bl2:
+		;add temp_blink, one
+		;cpi temp_blink, s1
+		;brlo skip_bl
+		;cbi Portb, green_pnum
+		;skip_bl:
+		;cpi temp_blink, s1 * 2
+		;brlo skip_bl2
+		;sbi portB, green_pnum
+		;skip_bl2:
 
 
 		cpi countHH, tim_m3HH
@@ -300,12 +335,17 @@ Start: ;_____________________________RESET:__________________________
 	
 	tout	TIMSK,	1 << TOIE1 |	0 << OCIE1A |	0 << TICIE1 |	1 << TOIE0
 
+	; general timer init ; set a time [ ]
+	ldi timeL, 255
+	ldi timeH, 255
+	ldi timeHH, 255
+
 ; pins int
 	; int0
 
 	tout	GIMSK,	0 << int1 |	1 << int0	; General Interrupt Mask
 	; MCU Control Register, Interrupt Sense Control
-	tout	MCUCR,	sleep_e << SE |	sleep_m << SM |	0 << ISC11 |	0 << ISC10 |	1 << ISC01 |	0 << ISC00
+	tout	MCUCR,	sleep_e |	sleep_m |	0 << ISC11 |	0 << ISC10 |	1 << ISC01 |	0 << ISC00
 	; ...Interrupt 0 Sense Control 10 - falling edge, 11 - rising, 00 - Low lvl; = MCUCR_pint0r
 
 ; sleep mode and pwr down
